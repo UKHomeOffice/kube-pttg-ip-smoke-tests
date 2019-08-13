21f9893492716d8e52b793b249abd158755df690
Owain Roberts <owain.roberts@bjss.com>
2019-08-13 14:16:49 +0100

    EE-6825 First attempt at a job spec for the smoke tests

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_SMOKE_TESTS_PR drone secret"
+    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_PR}
+elif [[ ${ENVIRONMENT} == "test" ]] ; then
+    echo "deploy ${VERSION} to test namespace, using PTTG_SMOKE_TESTS_TEST drone secret"
+    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_TEST}
+else
+    echo "deploy ${VERSION} to dev namespace, using PTTG_SMOKE_TESTS_DEV drone secret"
+    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_DEV}
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+if [ "${ENVIRONMENT}" == "pr" ] ; then
+    export DNS_PREFIX=
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+fi
+
+echo
+echo "Deploying pttg-ip-smoke-tests to ${ENVIRONMENT}"
+echo
+
+cd kd
+
+kd --insecure-skip-tls-verify -f job.yaml
