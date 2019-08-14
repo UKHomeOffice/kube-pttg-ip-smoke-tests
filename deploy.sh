c1f8548b2e49a614b4595cfc25d38402de940775
Owain Roberts <owain.roberts@bjss.com>
2019-08-14 09:03:58 +0100

    EE-6825 Changed Kube Token secret names to match other IPS projects

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
 
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
-    echo "deploy ${VERSION} to pr namespace, using PTTG_SMOKE_TESTS_PR drone secret"
-    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_PR}
+    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    export KUBE_TOKEN=${PTTG_IP_PR}
 elif [[ ${ENVIRONMENT} == "test" ]] ; then
-    echo "deploy ${VERSION} to test namespace, using PTTG_SMOKE_TESTS_TEST drone secret"
-    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_TEST}
+    echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+    export KUBE_TOKEN=${PTTG_IP_TEST}
 else
-    echo "deploy ${VERSION} to dev namespace, using PTTG_SMOKE_TESTS_DEV drone secret"
-    export KUBE_TOKEN=${PTTG_SMOKE_TESTS_DEV}
+    echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+    export KUBE_TOKEN=${PTTG_IP_DEV}
 fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
