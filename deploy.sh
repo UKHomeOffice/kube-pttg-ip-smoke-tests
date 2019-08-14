0f4c65455ccb3f1475f59097e829e6871731dea7
Owain Roberts <owain.roberts@bjss.com>
2019-08-14 17:30:10 +0100

    EE-6825 Fixing echo statements now that VERSION isn't configurable

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
 
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
-    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    echo "deploy smoke tests to pr namespace, using PTTG_IP_PR drone secret"
     export KUBE_TOKEN=${PTTG_IP_PR}
     export KUBE_SERVER=https://kube-api-prod.prod.acp.homeoffice.gov.uk
 elif [[ ${ENVIRONMENT} == "test" ]] ; then
-    echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+    echo "deploy smoke tests to test namespace, using PTTG_IP_TEST drone secret"
     export KUBE_TOKEN=${PTTG_IP_TEST}
 else
-    echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+    echo "deploy smoke tests to dev namespace, using PTTG_IP_DEV drone secret"
     export KUBE_TOKEN=${PTTG_IP_DEV}
 fi
 
